c616d7016fce8a3408dca10976f980a2592b9a7d
zaytech <zaytech@zayride.com>
2022-02-25 16:27:02 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { theme } from './src/core/theme'
+import {
+  AuthLoadingScreen,
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  ProfileScreen,
+  HomeScreen,
+} from './src/screens'
+import { FIREBASE_CONFIG } from './src/core/config'
+import { HomeIcon, ProfileIcon } from './src/assets/icons'
+import DrawerContent from './src/components/DrawerContent'
+
+const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(FIREBASE_CONFIG)
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="AuthLoadingScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="AuthLoadingScreen"
+            component={AuthLoadingScreen}
+          />
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="HomeScreen" component={DrawerNavigator} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function BottomNavigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => <HomeIcon fill={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => <ProfileIcon fill={color} />,
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator drawerContent={DrawerContent}>
+      <Drawer.Screen name="HomeScreen" component={BottomNavigation} />
+    </Drawer.Navigator>
+  )
+}
---
+++ src/core/config.js
@@ -0,0 +1,16 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyALArL-YtPk8v_y0tNJuUM5EEgVmx9Jnu0",
+  authDomain: "fir-login-a90b5.firebaseapp.com",
+  projectId: "fir-login-a90b5",
+  storageBucket: "fir-login-a90b5.appspot.com",
+  messagingSenderId: "649800159875",
+  appId: "1:649800159875:web:604353d0c923ae3e3341b6"
+}
+
+// Replace with your own IDs! follow the guides here:
+// https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
+export const ANDROID_GOOGLE_CLIENT_ID =
+'649800159875-fij7891nr40cpcl160efjf08vvc9f19q.apps.googleusercontent.com'
+export const IOS_GOOGLE_CLIENT_ID =
+  '878215484396-5hiqfjetgvbdck03l0jepdgku6u64erm.apps.googleusercontent.com'
